c78da048af768922703d6f0a6ad2421b3d0e0532
wagmicrew <wagmicrew@users.noreply.github.com>
2026-04-22 13:41:43 +0200

    fix: use solc 0.8.26 for oz mcopy compatibility

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: {
-    version: "0.8.24",
+    version: "0.8.26",
     settings: { optimizer: { enabled: true, runs: 200 } },
   },
   networks: {
